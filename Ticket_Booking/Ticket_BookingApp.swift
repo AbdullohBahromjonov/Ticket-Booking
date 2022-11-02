8ea64e831cb1eedb1c3efe46abe6787a8c580b0d
Abdulloh <abdullohbahromjonv@gmail.com>
2022-11-02 20:59:55 +0500

    Initial Commit

---
+++ Ticket_Booking/Ticket_BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ticket_BookingApp.swift
+//  Ticket_Booking
+//
+//  Created by Abdulloh on 02/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Ticket_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
